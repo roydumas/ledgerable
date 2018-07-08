eb931e1363d8fb3da37300338e7c8486339b3b7c
Roy Dumas <roydumas@gmail.com>
2018-07-07 19:01:15 -0700

    first commit

---
+++ hello-world/hello-world/wwwroot/DefaultController.cs
@@ -0,0 +1,16 @@
+﻿namespace hello_world.wwwroot
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [Route("hello-world")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
